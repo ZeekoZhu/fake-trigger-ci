55db1317888a4c1ff54305dacae5529c9ebf61fc
ZeekoZhu <vaezt@outlook.com>
2019-02-19 11:19:22 +0800

    Upgrade build group

--- build.fsx
+++ build.fsx
@@ -12,14 +12,18 @@ let MyFeeds = "https://www.myget.org/F/zeekoget/api/v2/package"
 
 let build () =
     Shell.cd "./src"
+    Trace.traceHeader "Removing previous build"
     Shell.rm_rf "./bin"
+    Trace.traceHeader "Building project"
     Utils.runCmd "dotnet" ["build"; "-c"; "Release"]
+    Trace.traceHeader "Packing nuget package"
     Utils.runCmd "paket" ["pack"; "bin/Release"]
 
 let pushPackage () =
     let nupkg =
         !! "bin/Release/*.nupkg"
         |> Seq.head
+    Trace.traceHeader "Pushing Package"
     Utils.runCmd "paket" ["push"; "--url"; MyFeeds; nupkg]
 
 Target.create "Build" (fun _ -> build ())
