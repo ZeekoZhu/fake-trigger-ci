eee64ea8bbd28b496aa69c231a63b1cadb955529
ZeekoZhu <vaezt@outlook.com>
2019-02-19 09:57:19 +0800

    Correct package dependencies

--- build.fsx
+++ build.fsx
@@ -1,15 +1,12 @@
 #load ".fake/build.fsx/intellisense.fsx"
+
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
-open Fake.Tools
-open CommandLine
 open Fake.MyFakeTools
-
-
 
 Target.create "Empty" ignore
 
